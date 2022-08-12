29648e26abbc5e0f5c074e0c42cb18cc20c1580b
Shahriyor <70831956+manperfecting@users.noreply.github.com>
2022-08-12 19:26:07 +0500

    feat: feat: implement 16/20 functional tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+  let countA = 0;
+  let countB = 0;
+  let countC = 0;
+  let countD = 0;
+  
+  for (let i = 0; i < str.length; i++){
+    if(str[i] == '('){
+      countA++
+    }if(str[i] == '['){
+      countB++
+    }if(str[i] == '{'){
+      countC++
+    }if(str[i] == '|'){
+      countD++
+    }if(str[i] == ')'){
+      if(countA == 0){
+        return false
+      }else{
+        countA--
+      }
+    }if(str[i] == ']'){
+      if(countB == 0){
+        return false
+      }else{
+        countB--
+      }
+    }if(str[i] == '}'){
+      if(countC == 0){
+        return false
+      }else{
+        countC--
+      }
+    }if(str[i] == '|'){
+      if(countD == 0){
+        return false
+      }else{
+        countD--
+      }
+    }
+  }
+  
+    if (countA == 0 && countB ==0 && countC == 0 && countD == 0){
+      return true
+    }else{
+      return false
+    }
 }
